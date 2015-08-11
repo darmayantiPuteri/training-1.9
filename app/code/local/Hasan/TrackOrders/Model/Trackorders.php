0affca091b98747a660dbafd9b846ce121228343
ahmad hasan <hasan@kolibrii.com>
2015-08-11 16:26:05 +0700

    track order2

---
+++ app/code/local/Hasan/TrackOrders/Model/Trackorders.php
@@ -0,0 +1,8 @@
+<?php 
+class Hasan_TrackOrders_Model_Trackorders extends Mage_Core_Model_Abstract {
+        protected function _construct()
+        {
+            $this->_init('hasan_trackorders/trackorders');
+        }     
+
+}
